7d27c7c05ca8af4b9a1aa056acc4e53e11a877f9
Catalina <CATALINA.ACUNAR@correoaiep.cl>
2021-03-03 21:46:48 -0300

    archivos modificados

--- login_app/models.py
+++ login_app/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 
 # Create your models here.
 from django.db import models
@@ -51,10 +50,10 @@ class UserManager(models.Manager):
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    alias = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = UserManager()
+    birthday = models.DateField(auto_now=True)
